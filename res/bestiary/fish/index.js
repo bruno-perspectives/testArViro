846b6db6e582f3db576aebc1dfadb41df5e6fec7
bruno marchasson <bruno@perspective-s.org>
2022-10-21 11:19:00 +0200

    a

--- res/bestiary/fish/index.js
+++ res/bestiary/fish/index.js
@@ -1,21 +1,24 @@
 export default {
   id: 'fish',
   anchor: require('./fish.png'),
-  source: require('./PoissonBonesTest.vrx'),
-  animation: {name: 'Take 001', run: true, loop: true},
+  source: require('./test.vrx'),
+  animation: {name: 'Spider_Armature|Attack', run: true, loop: true},
   //   source: require('./BarramundiFish.gltf'),
-  // resources: [
-  //   require('./object_bpanther_Base_Color.png'),
-  //   require('./object_bpanther_Metallic.png'),
-  //   require('./object_bpanther_Mixed_AO.png'),
-  //   require('./object_bpanther_Normal_OpenGL.png'),
-  //   require('./object_bpanther_Roughness.png'),
-  // ],
+  resources: [
+    require('./textures/haar_detail_NRM.jpg'),
+    require('./textures/SH3.png'),
+    require('./textures/Spinnen_Bein_tex_2.jpg'),
+    require('./textures/Spinnen_Bein_tex_COLOR_.jpg'),
+    require('./textures/Spinnen_Bein_tex_COLOR_.png'),
+    require('./textures/Spinnen_Bein_tex.jpg'),
+  ],
   // material: {
-  //   lightingModel: "PBR",
-  //   diffuseTexture: require('../tesla/object_car_main_Base_Color.png'),
-  //   metalnessTexture: require('../tesla/object_car_main_Metallic.png'),
-  //   roughnessTexture: require('../tesla/object_car_main_Roughness.png')
+    //   // lightingModel: "PBR",
+    // diffuseColor: require('./textures/SH3.png'),
+    //  transparencyFactor: require('./textures/SH3.png'),
+    //   // diffuseTexture: require('../tesla/object_car_main_Base_Color.png'),
+    //   // metalnessTexture: require('../tesla/object_car_main_Metallic.png'),
+    //   // roughnessTexture: require('../tesla/object_car_main_Roughness.png')
   // },
   type: 'VRX',
 };
